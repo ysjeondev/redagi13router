6df5e783d0c8ca60c4a4cd6bea420138fa8481e5
ysjeondev <hpc9909@gmail.com>
2026-03-26 10:49:03 +0900

    리다기 13장 334까지 완료

---
+++ src/info.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const info = () => {
+    return(
+        <div>
+            <h1>정보</h1>
+            <p>홈페이지의 정보를 보여지는 페이지</p>
+        </div>
+    )
+}
+
+export default info;
